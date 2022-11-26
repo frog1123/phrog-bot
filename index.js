08152c8b46cbd516de51bbed31f601069d86f8bd
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-11-26 14:30:39 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log('bot is online');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === 'ping') message.channel.send('pong');
+});
+
+client.login(process.env.TOKEN);
